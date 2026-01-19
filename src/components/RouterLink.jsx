e7c093dac9ba06aae876e045a18799b56da3d202
Roman Bilichenko <romabilichenko1@gmail.com>
2026-01-19 10:22:15 +0200

    fix: correct RouterLink href binding

--- src/components/RouterLink.jsx
+++ src/components/RouterLink.jsx
@@ -8,7 +8,7 @@ const RouterLink = (props) => {
   };
 
   return (
-    <a href="to" onClick={handleClick} {...rest}>
+    <a href={to} onClick={handleClick} {...rest}>
       {children}
     </a>
   );
